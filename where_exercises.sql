39ce6fce51ab087f5f1ef0d35d9ba8cc98a2e210
htrevino29 <htrevino29@gmail.com>
2016-09-20 10:01:01 -0500

    review of exercise.

--- where_exercises.sql
+++ where_exercises.sql
@@ -11,9 +11,11 @@
 					 
 					 
 /* STARTS and ENDS WITH E */					 				
-SELECT * FROM employees WHERE last_name LIKE 'E%'
-							AND last_name LIKE '%E';
+/* SELECT * FROM employees WHERE last_name LIKE 'E%'
+							AND last_name LIKE '%E'; */
 
--- SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01'AND '1999-12-31';
--- SELECT * FROM employees WHERE birth_date LIKE '%12-25';
--- SELECT * FROM employees WHERE last_name like '%q%';
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01'AND '1999-12-31' AND birth_date LIKE '%12-25';
+ 
+ 
+ 
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
