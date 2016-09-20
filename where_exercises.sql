997c04480815e6e0157bf79b7cf76cc62d0780c2
htrevino29 <htrevino29@gmail.com>
2016-09-20 09:47:41 -0500

    Now add a condition to find everybody with those names who is also male — 441 rows.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,8 +1,15 @@
 -- SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 -- changed to use OR.
-SELECT * FROM employees WHERE first_name = 'Irena'
+/* SELECT * FROM employees WHERE first_name = 'Irena'
 								    OR first_name = 'Vidya'
-								    OR first_name = 'Maya';
+								    OR first_name = 'Maya'; */
+-- Now add a condition to find everybody with those names who is also male — 441 rows.
+SELECT * FROM employees WHERE gender = 'M'
+						AND (first_name = 'Irena'
+						OR	first_name = 'Maya'
+						OR first_name = 'Vidya');
+					
+					
 -- SELECT * FROM employees WHERE last_name LIKE 'E%'
 
 -- SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01'AND '1999-12-31';
