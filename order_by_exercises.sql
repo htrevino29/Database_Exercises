3bb77d8bc3dbc82dd031bfc7d43e0912d2d3bf5b
htrevino29 <htrevino29@gmail.com>
2016-09-20 10:47:19 -0500

    Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -7,8 +7,11 @@ OR first_name = 'Vidya')
 ORDER BY last_name ASC, first_name ASC;
 					 					 
 /* STARTS and ENDS WITH E */					 				
-SELECT * FROM employees WHERE last_name LIKE 'E%'
-							AND last_name LIKE '%E';
+SELECT * FROM employees 
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+ORDER BY emp_no ASC;
+							
 -- BIRTHDAY RANGE AND 12-25
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01'AND '1999-12-31' AND birth_date LIKE '%12-25';
  
