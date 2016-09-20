83fad0a2c45ebd15ebeb0018c6161d9d306d6317
htrevino29 <htrevino29@gmail.com>
2016-09-20 10:41:48 -0500

    order_by_exercises step 1

---
+++ orderByNotes.sql
@@ -0,0 +1,1 @@
+orderByNotes.sql
---
+++ order_by_exercises.sql
@@ -0,0 +1,18 @@
+-- Now add a condition to find everybody with those names who is also male — 441 rows.
+SELECT * FROM employees 
+WHERE gender = 'M'
+AND (first_name = 'Irena'
+OR	first_name = 'Maya'
+OR first_name = 'Vidya')
+ORDER BY first_name ASC;
+					 
+					 
+/* STARTS and ENDS WITH E */					 				
+SELECT * FROM employees WHERE last_name LIKE 'E%'
+							AND last_name LIKE '%E';
+-- BIRTHDAY RANGE AND 12-25
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01'AND '1999-12-31' AND birth_date LIKE '%12-25';
+ 
+ 
+-- LAST NAME QITH Q WITHOUT QU
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
