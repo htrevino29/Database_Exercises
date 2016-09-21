fb4b7c20d3b0e7efe98b2d2c3bf7a55800d956fc
htrevino29 <htrevino29@gmail.com>
2016-09-21 16:17:27 -0500

    3rd problem solved.

--- join_databases_employee_examples.sql
+++ join_databases_employee_examples.sql
@@ -15,3 +15,19 @@ JOIN departments AS d
   ON d.dept_no = dm.dept_no
 WHERE dm.to_date = "9999-01-01" AND e.gender ='F';
 
+
+
+
+SELECT t.title,count(t.emp_no)
+FROM titles AS t
+JOIN dept_emp AS de
+ON de.emp_no = t.emp_no
+JOIN departments AS d
+ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'customer service' AND t.to_date = '9999-01-01'
+GROUP BY t.title;
+
+
+
+
+
