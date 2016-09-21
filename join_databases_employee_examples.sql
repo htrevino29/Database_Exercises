67f78c3793144daf38a8bf4616a23c9393519a78
htrevino29 <htrevino29@gmail.com>
2016-09-21 17:00:58 -0500

    boom!

--- join_databases_employee_examples.sql
+++ join_databases_employee_examples.sql
@@ -24,8 +24,42 @@ JOIN dept_emp AS de
 ON de.emp_no = t.emp_no
 JOIN departments AS d
 ON d.dept_no = de.dept_no
-WHERE d.dept_name = 'customer service' AND t.to_date = '9999-01-01'
+WHERE d.dept_name = 'customer service' AND t.to_date = '9999-01-01' AND de.to_date = '9999-01-01'
 GROUP BY t.title;
+
+SELECT d.dept_name AS 'DEPARTMENT NAME', CONCAT(e.first_name, ' ',e.last_name) AS 'manager name', s.salary AS 'salary'
+FROM departments AS d
+JOIN dept_manager AS dm
+	ON d.dept_no = dm.dept_no
+JOIN employees AS e
+	ON e.emp_no = dm.emp_no
+JOIN salaries AS s
+	ON e.emp_no = s.emp_no
+WHERE dm.t_date = '9999-01-01'
+	AND s.to_date = '9999-01-01';
+	
+		
+/* -bonus */
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name', d.dept_name AS 'Department Name', CONCAT(dme.first_name,' ',dme.last_name) AS 'Manager\'s Name'
+FROM employees AS e
+JOIN dept_emp AS de
+	ON e.emp_no = de.emp_no
+JOIN departments AS d
+	ON de.dept_no = d.dept_no
+JOIN dept_manager AS dm
+	ON d.dept_no = dm.dept_no
+JOIN employees AS dme
+	ON dme.emp_no = dm.emp_no
+WHERE de.to_date = '9999-01-01'
+	AND dm.to_date = '9999-01-01';
+
+
+
+
+
+
+
 
 
 
